a488e5df53645e5e8ded9cb80300045e4a07fee7
frank <www.417329062@qq.com>
2021-11-30 13:42:50 +0800

    add instructions

--- hello-solidity/005_ReentranceAttack.sol
+++ hello-solidity/005_ReentranceAttack.sol
@@ -13,6 +13,10 @@ contract EtherStore {
         uint bal = balances[msg.sender];
         require(bal > 0);
 
+        // call 方法会有重入攻击风险，因为 call 要消耗 gas 会执行 fallback 来确定使用多少 gas
+        // 攻击者可以在 fallback 里再次调用  withdraw , 因为通过合约调用 withdraw 这里的 msg.sender 最终是合约的地址，所以 bal 一直会大于 0
+        // 这里可以改用 transfer 或 send 方法， 或者使用加锁来防止重入攻击。
+        // 因为 transfer 和 send 方法只消耗 2300 gwei gas 不足够用来执行一个合约。
         (bool sent, ) = msg.sender.call{value: bal}("");
         require(sent, "Failed to send Ether");
 
