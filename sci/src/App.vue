97e3ecca38b906d084a822dea5d5dfcab4dbfcad
frank <www.417329062@qq.com>
2022-04-11 11:11:38 +0800

    first commit science+ project

---
+++ sci/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+</template>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
